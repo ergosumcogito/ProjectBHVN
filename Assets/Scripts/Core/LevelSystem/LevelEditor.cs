9abff2f837047279f2d9ec5d0101393c22cb6dbf
Lei <lfu813@gmail.com>
2025-12-19 18:55:10 +0100

    made level System functioning again

--- Assets/Scripts/Core/LevelSystem/LevelEditor.cs
+++ Assets/Scripts/Core/LevelSystem/LevelEditor.cs
@@ -8,16 +8,25 @@ public class LevelEditor : MonoBehaviour
     public int Width => levelData.width;
     public int Length => levelData.length;
     
+    public void ClearLevel()
+    {
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+        Debug.Log("All Tiles destroyed");
+    }
+    
     public void LoadAndStart(LevelData data)
     {
         levelData = data;
         GenerateLevel();
     }
 
+    // ReSharper disable Unity.PerformanceAnalysis
     public void GenerateLevel()
     {
+        ClearLevel();
         //Check if TilePrefab or BorderPrefab equals null
-        if (levelData.tilePrefab == null || levelData.borderPrefab == null)
+        if (!levelData.tilePrefab || !levelData.borderPrefab)
         {
             Debug.Log("TilePrefab or BorderPrefab is null");
         }
@@ -42,13 +51,13 @@ public class LevelEditor : MonoBehaviour
                 if (x == -1 || x == levelData.width || y == -1 || y == levelData.length)
                 {
                     Vector3 pos = new Vector3(x * levelData.tileSize, y * levelData.tileSize, 0);
-                    GameObject tile = Instantiate(levelData.borderPrefab, pos, Quaternion.identity);
+                    GameObject tile = Instantiate(levelData.borderPrefab, pos, Quaternion.identity, transform);
                     tile.name = $"Border x={x}, y={y}";
                 }
                 else
                 {
                     Vector3 pos = new Vector3(x * levelData.tileSize, y * levelData.tileSize, 0);
-                    GameObject tile = Instantiate(levelData.tilePrefab, pos, Quaternion.identity);
+                    GameObject tile = Instantiate(levelData.tilePrefab, pos, Quaternion.identity, transform);
                     tile.name = $"Tile x={x}, y={y}";
                 }
             }
