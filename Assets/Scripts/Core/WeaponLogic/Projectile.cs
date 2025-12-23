4869b3873964406e5cd2ecaba73d636638b5206b
ergosumcogito <ergosumcogito@users.noreply.github.com>
2025-12-23 13:33:27 +0100

    Don't delete enemy manually

--- Assets/Scripts/Core/WeaponLogic/Projectile.cs
+++ Assets/Scripts/Core/WeaponLogic/Projectile.cs
@@ -26,7 +26,7 @@ public class Projectile : MonoBehaviour
         if (enemy == null) return;
         
         enemy.TakeDamage(damage);
-        Destroy(gameObject);
+       // Destroy(gameObject); // TODO remove this when enemy.Die works properly
     }
 
 }
