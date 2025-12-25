79cfdfcebc1f24873d5d9d744cdc1450f8a6b314
Lei <lfu813@gmail.com>
2025-12-25 17:26:11 +0100

    added loadNextLevel method and goToNextLevel Method

--- Assets/Scripts/Core/LevelSystem/LevelManager.cs
+++ Assets/Scripts/Core/LevelSystem/LevelManager.cs
@@ -12,6 +12,9 @@ public class LevelManager : MonoBehaviour
     public int currentStageIndex = 1;
     public int currentLevelIndex = 1;
 
+    [HideInInspector]
+    public LevelData nextLevelData;
+    
     void Start()
     {
         if (masterConfig == null || levelEditor == null)
@@ -24,21 +27,52 @@ public class LevelManager : MonoBehaviour
 
     public void LoadCurrentLevel()
     {
-        StageConfig currentStage = masterConfig.stages[currentStageIndex-1];
-        LevelData levelData = currentStage.levels[currentLevelIndex-1];
+        StageConfig currentStage = masterConfig.stages[currentStageIndex - 1];
+        LevelData levelData = currentStage.levels[currentLevelIndex - 1];
         
         if (levelData != null)
         {
             levelEditor.LoadAndStart(levelData);
+            LoadNextLevel();
         }
         else
         {
-            Debug.LogError($"LevelData missing in Stage {currentStageIndex-1}, Level {currentLevelIndex-1}");
+            Debug.LogError($"LevelData missing in Stage {currentStageIndex - 1}, Level {currentLevelIndex - 1}");
         }
     }
     
     public void LoadNextLevel()
     {
+        int nextLevel = currentLevelIndex;
+        int nextStage = currentStageIndex - 1;
         
-    }     
+        if (nextLevel >= masterConfig.stages[nextStage].levels.Count)
+        {
+            nextLevel = 0;
+            nextStage++;
+        }
+        
+        if (nextStage >= masterConfig.stages.Count)
+        {
+            Debug.Log("No more stages available");
+        }
+        else
+        {
+           nextLevelData = masterConfig.stages[nextStage].levels[nextLevel];
+           Debug.Log("Next Level loaded");
+        }
+    }   
+    
+    public void GoToNextLevel()
+    {
+        if (nextLevelData !=null)
+        {
+            levelEditor.LoadAndStart(nextLevelData);
+            nextLevelData = null;
+        }
+        else
+        {
+            Debug.Log("No more stages and levels available");
+        }
+    }
 }
