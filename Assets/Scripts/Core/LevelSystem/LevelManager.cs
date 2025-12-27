7589b3bc08483071e4fd0c2be78207deabbc2772
ergosumcogito <ergosumcogito@users.noreply.github.com>
2025-12-27 14:46:37 +0100

    Rename load next level, remove load on start, delete deprecated methods

--- Assets/Scripts/Core/LevelSystem/LevelManager.cs
+++ Assets/Scripts/Core/LevelSystem/LevelManager.cs
@@ -26,7 +26,6 @@ public class LevelManager : MonoBehaviour
             Debug.LogError("MasterConfig or LevelEditor missing in LevelManager");
             return;
         }
-        LoadCurrentLevel();
     }
 
     public void LoadCurrentLevel()
@@ -39,7 +38,7 @@ public class LevelManager : MonoBehaviour
             if (levelData != null)
             {
                 levelEditor.LoadAndStart(levelData);
-                LoadNextLevel();
+                MoveToNextLevel(); // Automatic level increase (maybe call this manually in future)
             }
             else
             {
@@ -52,7 +51,7 @@ public class LevelManager : MonoBehaviour
         }
     }
     
-    public void LoadNextLevel()
+    public void MoveToNextLevel()
     {
         int nextLevel = CurrentLevelIndex + 1;
         int nextStage = CurrentStageIndex; 
@@ -75,48 +74,10 @@ public class LevelManager : MonoBehaviour
         else
         {
            nextLevelData = masterConfig.stages[nextStage].levels[nextLevel];
+           
            currentLevelDisplay = nextLevel + 1;
            currentStageDisplay = nextStage + 1;
            Debug.Log("Next Level loaded");
-        }
-    }
-
-    public void IncrementLevel()
-    {
-        if (masterConfig == null || masterConfig.stages.Count == 0)
-            return;
-
-        currentLevelDisplay++;
-
-        StageConfig currentStage = masterConfig.stages[CurrentStageIndex];
-
-        if (CurrentLevelIndex > currentStage.levels.Count)
-        {
-            currentLevelDisplay = 1;
-            currentStageDisplay++;
-
-            if (CurrentStageIndex > masterConfig.stages.Count)
-            {
-                currentStageDisplay = 1;
-            }
-        }
-
-        StageConfig nextStage = masterConfig.stages[CurrentStageIndex];
-        nextLevelData = nextStage.levels[CurrentLevelIndex];
-
-       // Debug.Log($"Incremented to Stage {currentStageIndex}, Level {currentLevelIndex}");
-    }
-
-    public void GoToNextLevel()
-    {
-        if (nextLevelData !=null)
-        {
-            levelEditor.LoadAndStart(nextLevelData);
-            nextLevelData = null;
-        }
-        else
-        {
-            Debug.Log("No more stages and levels available");
         }
     }
 }
