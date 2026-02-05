9fab548ec0d70222240c1c402c8be81256e29ad6
braiiiin <danylermann@web.de>
2026-02-05 15:57:27 +0100

    Prefab created
    Basic Prefab created. Script reworked. Weapon is now ready to be tested.

--- Assets/Scripts/Core/WeaponLogic/AuraWeapon.cs
+++ Assets/Scripts/Core/WeaponLogic/AuraWeapon.cs
@@ -14,32 +14,20 @@ namespace Core.WeaponLogic
 
         [SerializeField] private float tickDamageReduction = 2f; // Divides final dmg because Weapon hits multiple times per second
 
-        [SerializeField] private float meleeWeight = 1.5f; //The lower the number the higher the contribution
+        [SerializeField] private float meleeWeight = 1f; //The lower the number the higher the contribution
 
         [SerializeField] private float rangedWeight = 3f;
-
-        private List<EnemyAbstract> enemies = new List<EnemyAbstract>();
 
         // Update is called once per frame
         new void Update()
         {
             tick += Time.deltaTime;
-
-            if (tick >= tickTime)
-            {
-                tick = 0;
-                foreach (var enemy in enemies)
-                {
-                    enemy.TakeDamage(CalculateDamage());
-                }
-
-            }
         }
 
         public override float CalculateDamage()
         {
 
-            float dmg = (meleeDamage / meleeWeight + rangeDamage / rangedWeight) / tickDamageReduction; //Default Weapon damage is 2/3 Melee Damage and 1/3 Ranged Damage and then gets reduced by the tickDamageReduction. Calculation divides with the reciprocal of the intended distribution to avoid using fractions in code and allow easier modification even outside of code.
+            float dmg = (meleeDamage / meleeWeight + rangeDamage / rangedWeight) / tickDamageReduction; //Default Weapon damage is 1x Melee Damage + 1/3 Ranged Damage and then gets reduced by the tickDamageReduction. Calculation divides with the reciprocal of the intended distribution to avoid using fractions in code and allow easier modification even outside of code.
             
             return CalculateCrit(dmg);
         }
@@ -49,21 +37,14 @@ namespace Core.WeaponLogic
             throw new NotImplementedException();
         }
 
-        public void OnTriggerEnter2D(Collider2D collide)
+        private void OnTriggerStay2D(Collider2D collide)
         {
-            EnemyAbstract enemy = collide.GetComponent<EnemyAbstract>();
-            if (enemy == null) return;
-            
-            enemies.Add(enemy);
-
-        }
-
-        private void OnTriggerExit2D(Collider2D collide)
-        {
-            EnemyAbstract enemy = collide.GetComponent<EnemyAbstract>();
-            if (enemy == null) return;
-
-            enemies.Remove(enemy);
+            if (tick >= tickTime)
+            {
+                EnemyAbstract enemy = collide.GetComponent<EnemyAbstract>();
+                enemy.TakeDamage(CalculateDamage());
+                tick = 0;
+            }
         }
     }
 }
