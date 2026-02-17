ad1dfb9ee5cf77a4d157227a9fd77f83a8c5da28
siviee <silvia.anghel275@gmail.com>
2026-02-17 23:10:15 +0100

    Added Projectile for Mushroom

--- Assets/Scripts/Core/Enemy-Logic/Mushroom.cs
+++ Assets/Scripts/Core/Enemy-Logic/Mushroom.cs
@@ -6,31 +6,52 @@ namespace Core.Enemy_Logic
     public class Mushroom : EnemyAbstract
     {
         [SerializeField] private List<GameObject> drops;
-
         [SerializeField] private float maxHealth = 70f;
-        [SerializeField] private float attackPower = 25f;
         [SerializeField] private float moveSpeed = 3f;
-        [SerializeField] private float attackRange = 3.5f;
-        [SerializeField] private float cooldown = 2f;
-        [SerializeField] private float spawnFadeTime = 0.002f;
+        [SerializeField] private float attackRange = -1f;
+        [SerializeField] private float spawnFadeTime = 2f;
 
         [SerializeField] private int coinMin = 10;
         [SerializeField] private int coinMax = 20;
 
+        [Header("Mushroom behaviour")] 
+        [SerializeField] private bool isFleeingType = true;
+        [SerializeField] private float idleMinDistance = 20f;
+        [SerializeField] private float idleMaxDistance = 30f;
+        [SerializeField] private int escapeCornerSpeedMultiplier = 5;
+        [SerializeField] private bool canTeleportBehindPlayer;
+        
+        [Header("Necromancer projectiles attack")]
+        [SerializeField] private bool firesProjectiles = true;
+        [SerializeField] private GameObject projectile;
+        [SerializeField] private float attackPower = 25f;
+        [SerializeField] private float projectileSpeed = 12f;
+        [SerializeField] private float cooldown = 5f;
+        [SerializeField] private float firstAttackDelay = 5f;
         protected override void Awake()
         {
             MaxHealth = maxHealth;
-            AttackPower = attackPower;
-            MoveSpeed = moveSpeed;
             AttackRange = attackRange;
-            Cooldown = cooldown;
-            SpawnFadeTime = spawnFadeTime;
-            Drops = drops;
+            MoveSpeed = moveSpeed;
 
+            FiresProjectiles = firesProjectiles;
+            Projectile = projectile;
+            AttackPower = attackPower;
+            ProjectileSpeed = projectileSpeed;
+            Cooldown = cooldown;
+            
+            IsFleeingType = isFleeingType;
+            IdleMinDistance = idleMinDistance;
+            IdleMaxDistance = idleMaxDistance;
+            SpawnFadeTime = spawnFadeTime;
+            EscapeCornerSpeedMultiplier = escapeCornerSpeedMultiplier;
+            CanTeleportBehindPlayer = canTeleportBehindPlayer;
+            Drops = drops;
             CoinMin = coinMin;
             CoinMax = coinMax;
+            
 
-            base.Awake(); // currentHealth already declared in the EnemyAbstract
+            base.Awake();
         }
     }
 }
