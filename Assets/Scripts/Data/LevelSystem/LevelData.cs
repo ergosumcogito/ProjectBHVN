c7f499163a0fcc63c6cd70bcd33f0953122faa2a
LeiFuCoding <157929483+LeiFuCode@users.noreply.github.com>
2026-02-21 17:22:18 +0100

    changed levelsize to min 5 and max 100

--- Assets/Scripts/Data/LevelSystem/LevelData.cs
+++ Assets/Scripts/Data/LevelSystem/LevelData.cs
@@ -9,8 +9,12 @@ public class LevelData : ScriptableObject
     public LevelType levelType = LevelType.Normal;
     
     [Header("Level Size Settings")]
+    
+    [Range(5, 100)]
     public int width = 10;
+    [Range(5, 100)]
     public int height = 10;
+    
     public float tileSize = 1f;
     
     [Header("Floor Tiles")]
