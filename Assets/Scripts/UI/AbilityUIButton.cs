e86af8f92d7b4afcc1f0bf29d3e82f8ed447fca1
ergosumcogito <ergosumcogito@users.noreply.github.com>
2026-02-23 13:20:12 +0100

    Add cooldown and grey-out functionality

--- Assets/Scripts/UI/AbilityUIButton.cs
+++ Assets/Scripts/UI/AbilityUIButton.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Core.PlayerLogic.Abilities;
 
 public class AbilityUIButton : MonoBehaviour
 {
@@ -8,7 +11,73 @@ public class AbilityUIButton : MonoBehaviour
         Heal
     }
 
+    [Header("Config")]
     [SerializeField] private AbilityType abilityType;
+
+    [Header("UI")]
+    [SerializeField] private Button button;
+    [SerializeField] private Image buttonImage;
+    [SerializeField] private TextMeshProUGUI cooldownText;
+
+    private PlayerAbilitySlot abilitySlot;
+    private Color _defaultColor;
+
+    private void Awake()
+    {
+        if (buttonImage == null)
+            buttonImage = button.GetComponent<Image>();
+
+        _defaultColor = buttonImage.color;
+    }
+
+    private void OnEnable()
+    {
+        TryInitialize();
+    }
+
+    private void Update()
+    {
+        if (abilitySlot == null)
+        {
+            TryInitialize();
+            return;
+        }
+
+        UpdateCooldownVisual();
+    }
+
+    private void TryInitialize()
+    {
+        abilitySlot = null;
+
+        var slots = FindObjectsOfType<PlayerAbilitySlot>();
+        
+        // TODO debug
+        Debug.Log($"[{abilityType}] Found {slots.Length} ability slots in scene 2281337");
+
+
+        foreach (var slot in slots)
+        {
+            if (abilityType == AbilityType.Flash &&
+                slot.Ability is FlashAbility)
+            {
+                abilitySlot = slot;
+                Debug.Log($"[{abilityType}] Matched FlashAbility slot 2281337");
+                break;
+            }
+
+            if (abilityType == AbilityType.Heal &&
+                slot.Ability is HealAbility)
+            {
+                abilitySlot = slot;
+                Debug.Log($"[{abilityType}] Matched HealAbility slot 2281337");
+                break;
+            }
+        }
+        
+        if (abilitySlot == null)
+            Debug.LogWarning($"[{abilityType}] No ability slot found! 2281337");
+    }
 
     public void OnClick()
     {
@@ -17,5 +86,30 @@ public class AbilityUIButton : MonoBehaviour
 
         if (abilityType == AbilityType.Heal)
             InputReader.Instance.TriggerHeal();
+    }
+
+    private void UpdateCooldownVisual()
+    {
+        float remaining = abilitySlot.CooldownRemaining;
+
+        if (remaining > 0f)
+        {
+            button.interactable = false;
+
+            // Fade button image manually
+            var c = _defaultColor;
+            c.a = 0.6f;
+            buttonImage.color = c;
+
+            cooldownText.gameObject.SetActive(true);
+            cooldownText.text = Mathf.CeilToInt(remaining).ToString();
+        }
+        else
+        {
+            button.interactable = true;
+            buttonImage.color = _defaultColor;
+
+            cooldownText.gameObject.SetActive(false);
+        }
     }
 }
