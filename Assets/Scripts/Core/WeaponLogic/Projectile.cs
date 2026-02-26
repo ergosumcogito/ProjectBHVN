99a3308ab5d146c8222178fa6f9a8d0e83ef501e
ergosumcogito <ergosumcogito@users.noreply.github.com>
2026-02-26 15:55:55 +0100

    Add fixed lifetime to projectile

--- Assets/Scripts/Core/WeaponLogic/Projectile.cs
+++ Assets/Scripts/Core/WeaponLogic/Projectile.cs
@@ -8,11 +8,16 @@ public class Projectile : MonoBehaviour
     private float critChance;
     private Vector3 direction;
 
+    [SerializeField] private float lifeTime = 5f; // default lifetime
+    private float lifeTimer;
+
     public void Init(Vector3 dir, float speed, float damage)
     {
         this.direction = dir.normalized;
         this.speed = speed;
         this.damage = damage;
+
+        lifeTimer = 0f; // reset timer on spawn
 
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
@@ -21,12 +26,19 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         transform.position += direction * speed * Time.deltaTime;
+
+        lifeTimer += Time.deltaTime;
+        if (lifeTimer >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         EnemyAbstract enemy = collision.GetComponentInParent<EnemyAbstract>();
         if (enemy == null) return;
+
         if (!enemy.IsTargattable)
         {
             Destroy(gameObject);
