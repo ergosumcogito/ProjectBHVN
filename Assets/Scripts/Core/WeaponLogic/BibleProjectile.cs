fdaa91697cfd1961e37e6dd9a72862d8dc2f548e
braiiiin <danylermann@web.de>
2026-02-26 20:52:34 +0100

    Bible projectile

---
+++ Assets/Scripts/Core/WeaponLogic/BibleProjectile.cs
@@ -0,0 +1,47 @@
+using Core.Enemy_Logic;
+using UnityEngine;
+
+public class BibleProjectile : MonoBehaviour
+{
+    private float speed;
+    private float damage;
+    private float critChance;
+    [SerializeField] private float lifeTime = 5f; // default lifetime
+    private float lifeTimer;
+    private Transform player;
+    public Vector3 rotationAxis = Vector3.up;
+    
+    public void Init(float speed, float damage, float attackSpeed, Vector3 position, Transform player)
+    {
+        this.speed = speed;
+        this.damage = damage;
+
+        lifeTimer = 0f; // reset timer on spawn
+        lifeTime /= attackSpeed + 1;
+        GetComponent<Transform>().position = position;
+        this.player = player;
+    }
+
+    void Update()
+    {
+        lifeTimer += Time.deltaTime;
+        if (lifeTimer >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+
+        if (player != null)
+        {
+            transform.RotateAround(player.position, rotationAxis, speed * Time.deltaTime);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyAbstract enemy = collision.GetComponentInParent<EnemyAbstract>();
+        if (enemy == null) return;
+
+        enemy.TakeDamage(damage);
+    }
+    
+}
